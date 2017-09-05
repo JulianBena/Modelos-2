093a17594227deace5e03513309eec4d93a35ea0
juklian <30881080+juklian@users.noreply.github.com>
2017-09-05 13:32:03 -0500

    Update Ejercicios 1.hs

--- Haskell/Ejercicios 1.hs
+++ Haskell/Ejercicios 1.hs
@@ -8,4 +8,13 @@ dividir::Int->Int->Int
 dividir a 0 = 0
 dividir 0 b = 0
 dividir a b = if a < b then 0 
-              else 1 + dividir (a - b) b
+else 1 + dividir (a - b) b
+
+converbin::Int->Int
+converbin 1 = 1
+converbin a = (conbin((div a 2)) * 10) + (mod a 2)
+
+
+converdec::Int->Int
+converdec a = if a < 2 then a
+converdec a = if a > 2 then (converdec((div a 10)) * 2 ) +(mod a 10)
