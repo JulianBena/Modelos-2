8eadfb039ef3db9afa295308643d9752bf5ae037
juklian <30881080+juklian@users.noreply.github.com>
2017-09-11 22:42:58 -0500

    Update Ejercicios.hs

--- Haskell/Ejercicios.hs
+++ Haskell/Ejercicios.hs
@@ -12,8 +12,16 @@ else 1 + dividir (a - b) b
 
 converbin::Int->Int
 converbin 1 = 1
-converbin a = (conbin((div a 2)) * 10) + (mod a 2)
+converbin a = (converbin((div a 2)) * 10) + (mod a 2)
 
 converdec::Int->Int
 converdec a = if a < 2 then a
-converdec a = if a > 2 then (converdec((div a 10)) * 2 ) +(mod a 10)
+else (converdec((div a 10)) * 2 ) +(mod a 10)
+
+capicua::Int->Bool
+capicua a 
+ | a > 0 && a < 10 = True
+ | a > 9999 = error "argumento mayor que 9999"
+ | a > 999 = ((div a 1000) == (mod a 10)) && ((mod (div a 100) 10) == (div (mod a 100) 10))
+ | a > 99 = (a==(((mod a 10)*10^2)+((mod (div a 10) 10)*10)+(mod (div a 100) 10)))
+ | a > 9 && a <= 99 = ((mod a 11) == 0)
