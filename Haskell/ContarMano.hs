b3d70c423202d8b4294fb287b9812800469228bd
juklian <30881080+juklian@users.noreply.github.com>
2017-09-14 19:13:39 -0500

    Update ContarMano.hs

--- Haskell/ContarMano.hs
+++ Haskell/ContarMano.hs
@@ -1,6 +1,6 @@
-contarMano::[(Char,Char)]->Int 
+contarMano::[(Char,Char)]->Int
 contarMano [ ] = 0
-contarMano ((a,b):xs) 
+contarMano ((a,b):xs)
  | a == 'A' = 1 + contarMano(xs)
  | a == '2' = 2 + contarMano(xs)
  | a == '3' = 3 + contarMano(xs)
@@ -19,9 +19,11 @@ buscarAs::[(Char,Char)]->Bool
 buscarAs [ ] = False
 buscarAs ((a,b):xs)
  | a == 'A' = True
+ | a /= 'A' = buscarAs xs
 
 contarAs::[(Char,Char)]->Int
-contarAs [ ] = 0
+contarAs [] = 0
 contarAs ((a,b):xs)
  | (buscarAs ((a,b):xs) == True && contarMano ((a,b):xs) > 11 ) = contarMano ((a,b):xs)
  | (buscarAs ((a,b):xs) == True && contarMano ((a,b):xs) <= 11 ) = contarMano ((a,b):xs) + 10
+ | otherwise = contarMano ((a,b):xs)
